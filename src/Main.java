dd065c606b3b456ddbc44b468c11f8418630715b
Ashish-001 <57615541+Ashish-001@users.noreply.github.com>
2024-03-02 20:08:34 +0100

    Factory Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import DesignPatterns.Samosa;
-import DesignPatterns.callMethod;
+import DesignPatterns.FactoryDesign.CombinedClass;
+import DesignPatterns.FactoryDesign.Factory;
+import DesignPatterns.Singelton.Samosa;
 
 import java.lang.reflect.Constructor;
-import java.util.*;
-import java.io.*;
 import java.lang.*;
 
 public class Main extends Check{
@@ -11,13 +10,16 @@ public class Main extends Check{
 //    Check check = new Check();
 //    check.area();
 //    check.maxLength();
-        System.out.println(Samosa.getSamosa().hashCode());
+//        System.out.println(Samosa.getSamosa().hashCode());
 
         // Methods to break Singelton
-        Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        Samosa s2 = constructor.newInstance();
-        System.out.println(s2.hashCode());
+//        Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
+//        constructor.setAccessible(true);
+//        Samosa s2 = constructor.newInstance();
+//        System.out.println(s2.hashCode());
+        Factory FACTORY= CombinedClass.getFactory("Manufacturing");
+        FACTORY.salary();
+
     }
 }
 
