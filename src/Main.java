b43ba83b6009c2513726567ebede4841bf01937f
Ashish-001 <57615541+Ashish-001@users.noreply.github.com>
2024-03-02 19:41:41 +0100

    include this as well

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
+import DesignPatterns.Samosa;
+import DesignPatterns.callMethod;
+
+import java.lang.reflect.Constructor;
 import java.util.*;
 import java.io.*;
 import java.lang.*;
 
 public class Main extends Check{
-    public static void main(String[] args){
-    Check check = new Check();
-    check.area();
-    check.maxLength();
+    public static void main(String[] args) throws Exception{
+//    Check check = new Check();
+//    check.area();
+//    check.maxLength();
+        System.out.println(Samosa.getSamosa().hashCode());
+
+        // Methods to break Singelton
+        Constructor<Samosa> constructor = Samosa.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Samosa s2 = constructor.newInstance();
+        System.out.println(s2.hashCode());
     }
 }
 
